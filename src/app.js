964cf8ab41cb9c8ed665a37093b156d293d9557e
satyamjha2001 <satyamjha23112001@gmail.com>
2025-10-25 11:06:44 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.use("/route", rH1, [rH2, rH3], rH4, rH5);
 app.get(
   "/user",
   (req, res, next) => {
@@ -16,19 +17,21 @@ app.get(
     // res.send("2nd Response!!");
     next();
   },
-  (req, res, next) => {
-    console.log("Handling the route user 3!!");
-    // res.send("3rd Response!!");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 4!!");
-    // res.send("4th Response!!");
-    next();
-  },
+  [
+    (req, res, next) => {
+      console.log("Handling the route user 3!!");
+      // res.send("3rd Response!!");
+      next();
+    },
+    (req, res, next) => {
+      console.log("Handling the route user 4!!");
+      // res.send("4th Response!!");
+      next();
+    },
+  ],
   (req, res, next) => {
     console.log("Handling the route user 5!!");
-    res.send("5th Response!!");  //when all response comment then it is hanged
+    res.send("5th Response!!"); //when all response comment then it is hanged
     // next(); //when this is uncommented then it gives error because no more middleware to handle
   }
 );
